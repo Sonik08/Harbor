dc039cbb507adb98f8803a93635f6716f3a8a0db
Stanislav Novikov <novikovs@cbs.gr>
2021-06-26 19:52:45 +0300

    Disabling bugged AOT Ivy Angular compiler

--- src/app/modules/gas-station/gas-station-overview/gas-station-overview.module.ts
+++ src/app/modules/gas-station/gas-station-overview/gas-station-overview.module.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { ListModule } from 'src/app/base-components/list/list.module';
 import { GasStationOverviewComponent } from './gas-station-overview.component';
 
 @NgModule({
   declarations: [GasStationOverviewComponent],
-  imports: [CommonModule, ListModule, MatIconModule]
+  imports: [CommonModule, ListModule]
 })
 export class GasStationOverviewModule {}
