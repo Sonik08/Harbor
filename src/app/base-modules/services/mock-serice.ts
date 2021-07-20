b1b0756fac8b369ed8d9993d9a6ac30ec2d84c0a
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-20 12:27:03 +0300

    CORE Adding gas stations array to mock service

--- src/app/base-modules/services/mock-serice.ts
+++ src/app/base-modules/services/mock-serice.ts
@@ -1,40 +1,60 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { GasStation } from 'src/app/app-modules/models/gas-station';
 import { IListItem } from 'src/app/base-modules/models/list-item';
 
 @Injectable({ providedIn: 'root' })
 export class MockService {
+
+  gasStations: GasStation[] = [
+    {
+      id: '123',
+      adress: "Liosiwn 10",
+      name: 'hello world',
+      shifts: []
+    },
+    {
+      id: '121321',
+      adress: "Tatoiou 94",
+      name: 'BP',
+      shifts: []
+    },
+    {
+      id: '3242',
+      adress: "Kurumba 123",
+      name: 'fdsfdsf',
+      shifts: []
+    },
+    {
+      id: '111',
+      adress: "Manhatan bliat 12",
+      name: 'eteka',
+      shifts: []
+    }
+  ];
+
+  gasStationsTableColumns : string[]  = ['id', 'name'];
+
+  gasStationListItems: IListItem[] = [
+    { title: 'Fuel Tank Levels', icon: 'euro', url: ''},
+    { title: 'New Shift', icon: 'person', url: './' }
+  ];
+
   getGasStations(): GasStation[] {
-    return [
-      {
-        id: '123',
-        name: 'hello world'
-      },
-      {
-        id: '121321',
-        name: 'BP'
-      },
-      {
-        id: '3242',
-        name: 'fdsfdsf'
-      },
-      {
-        id: '111',
-        name: 'eteka'
-      }
-    ];
+    return this.gasStations;
   }
 
   getGasStationTableColumns(): string[] {
-    return ['id', 'name'];
+    return this.gasStationsTableColumns
   }
 
   getGasStationListItems(): IListItem[] {
-    return [
-      { title: 'Income', icon: 'euro', url: ''},
-      { title: 'Income per shift', icon: 'person', url: './' },
-      { title: 'Income per day', icon: 'group', url: './' }
-    ];
+    return this.gasStationListItems;
+  }
+
+  getGasStationById(id: string): Observable<GasStation>{
+    return of(this.gasStations
+      .filter(gasStation => gasStation.id === id)[0]);
   }
 
   getExpantionPanelItems(){
