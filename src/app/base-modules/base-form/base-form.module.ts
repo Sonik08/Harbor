7d0c7280a34efdc4af1650a90f80e2885dd42c37
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-20 16:06:36 +0300

    CORE adding base-form module

---
+++ src/app/base-modules/base-form/base-form.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BaseFormComponent } from './base-form.component';
+
+
+
+@NgModule({
+  declarations: [BaseFormComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BaseFormModule { }
