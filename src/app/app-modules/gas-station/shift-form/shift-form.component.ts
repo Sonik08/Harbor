569ba0b9f3242f758646faccedc8fe084a8d25f9
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-20 16:01:04 +0300

    Gas Station Adding shift form

---
+++ src/app/app-modules/gas-station/shift-form/shift-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shift-form',
+  templateUrl: './shift-form.component.html',
+  styleUrls: ['./shift-form.component.scss']
+})
+export class ShiftFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
