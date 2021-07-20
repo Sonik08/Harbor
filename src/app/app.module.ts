70a9c270eaacecddbf8c3fd305b46df735b1b6ab
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-20 19:04:11 +0300

    CORE removing formly module for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './pages/layout/layout.module';
+import { ControlDirective } from './core/controls/control.directive';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ControlDirective],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent]
