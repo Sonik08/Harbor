6a12eda45307c3bf2efc16b3949a3d0203779b3d
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-20 19:03:45 +0300

    CORE Adding base control directive

---
+++ src/app/core/controls/control.directive.ts
@@ -0,0 +1,46 @@
+import { Directive, Input } from "@angular/core";
+import { Validators, FormGroup, FormControl } from "@angular/forms";
+
+@Directive()
+export abstract class Control {
+  validations = [
+    {
+      type: Validators.pattern.name,
+      message: "Λανθασμένη μορφή"
+    }
+  ];
+
+  @Input()
+  form: FormGroup;
+
+  @Input()
+  name: string;
+
+  @Input()
+  label: string;
+
+  @Input()
+  readonly = false;
+
+  @Input()
+  required = false;
+
+  get control(): FormControl {
+    return this.form.controls[this.name] as FormControl;
+  }
+
+  get validationMessages(): string[] {
+    const messages: string[] = [];
+
+    this.validations.forEach(validation => {
+      if (
+        this.control.hasError(validation.type) &&
+        (this.control.dirty || this.control.touched)
+      ) {
+        messages.push(validation.message);
+      }
+    });
+
+    return messages;
+  }
+}
