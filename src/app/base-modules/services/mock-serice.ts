aa3ebf64f674278eb64dd96c2b0356e01f8163f8
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-20 16:07:11 +0300

    Gas Station adding shift to mock service

--- src/app/base-modules/services/mock-serice.ts
+++ src/app/base-modules/services/mock-serice.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { ExpenseItem } from 'src/app/app-modules/models/expense-item';
+import { ExpenseType } from 'src/app/app-modules/models/expense-type';
+import { FuelType } from 'src/app/app-modules/models/fuel-type';
 import { GasStation } from 'src/app/app-modules/models/gas-station';
+import { Shift } from 'src/app/app-modules/models/shift';
+import { ShiftType } from 'src/app/app-modules/models/shift-type';
 import { IListItem } from 'src/app/base-modules/models/list-item';
 
 @Injectable({ providedIn: 'root' })
@@ -32,12 +37,11 @@ export class MockService {
       shifts: []
     }
   ];
-
   gasStationsTableColumns : string[]  = ['id', 'name'];
-
   gasStationListItems: IListItem[] = [
     { title: 'Fuel Tank Levels', icon: 'euro', url: ''},
-    { title: 'New Shift', icon: 'person', url: './' }
+    { title: 'New Shift', icon: 'person', url: 'shifts/new' },
+    { title: 'Shifts', icon: 'groups', url: 'shifts'}
   ];
 
   getGasStations(): GasStation[] {
@@ -54,6 +58,112 @@ export class MockService {
 
   getGasStationById(id: string): Observable<GasStation>{
     return of(this.gasStations
+      .filter(gasStation => gasStation.id === id)[0]);
+  }
+
+  shifts: Shift[] = [
+    {
+      id: "123",
+      type : ShiftType.Morning,
+      income: 500,
+      date: new Date(new Date().setDate(new Date().getDate()-1)).toLocaleString(),
+      expenseItems: [
+        {
+          shiftId: "123",
+          type: ExpenseType.Miscellaneous,
+          amount: 100
+        },
+        {
+          shiftId: "123",
+          type: ExpenseType.Salary,
+          amount: 30
+        }
+      ],
+      fuelLevels: [
+        {
+          shiftId: "123",
+          fuelType: FuelType.Diesel,
+          level: 920.1
+        },
+        {
+          shiftId: "123",
+          fuelType: FuelType.Unleaded,
+          level: 123.1
+        },
+        {
+          shiftId: "123",
+          fuelType: FuelType.LPG,
+          level: 424.1
+        }
+      ],
+      resupplies:[
+        {
+          fuelType: FuelType.Diesel,
+          amount: 500,
+          shiftId: "1234"
+        }
+      ],
+      gasStationId: "121321",
+      profit: 370
+    } as Shift,
+    {
+      id: "1234",
+      type : ShiftType.Morning,
+      income: 500,
+      date: new Date(new Date().setDate(new Date().getDate()-1)).toLocaleString(),
+      expenseItems: [
+        {
+          shiftId: "1234",
+          type: ExpenseType.Miscellaneous,
+          amount: 100
+        },
+        {
+          shiftId: "1234",
+          type: ExpenseType.Salary,
+          amount: 200
+        }
+      ],
+      fuelLevels: [
+        {
+          shiftId: "1234",
+          fuelType: FuelType.Diesel,
+          level: 1000.1
+        },
+        {
+          shiftId: "1234",
+          fuelType: FuelType.Unleaded,
+          level: 900.1
+        },
+        {
+          shiftId: "1234",
+          fuelType: FuelType.LPG,
+          level: 424.1
+        }
+      ],
+      resupplies:[
+        {
+          fuelType: FuelType.Diesel,
+          amount: 500,
+          shiftId: "1234"
+        }
+      ],
+      gasStationId: "123",
+      profit: 200
+    } as Shift
+  ]
+
+  shiftsTableColumns: string[] = ["type", "profit", "date", "gasStationId"]
+
+  getShifts(): Observable<Shift[]> {
+    return of(this.shifts);
+  }
+
+  getShiftsTableColumns(): Observable<string[]>{
+    return of(this.shiftsTableColumns);
+  }
+
+  getShiftById(id: string): Observable<Shift>{
+    return of(this.shifts
       .filter(gasStation => gasStation.id === id)[0]);
   }
 
