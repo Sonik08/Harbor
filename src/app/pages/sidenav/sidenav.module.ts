e4f9e587fb784f4f18caf572b9e5882567eac7a4
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-22 15:24:22 +0300

    PAGES Centering content in router

--- src/app/pages/sidenav/sidenav.module.ts
+++ src/app/pages/sidenav/sidenav.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
 import { SidenavComponent } from './sidenav.component';
 
 @NgModule({
   declarations: [SidenavComponent],
-  imports: [CommonModule, MaterialModule, RouterModule],
+  imports: [CommonModule, MaterialModule, RouterModule, FlexLayoutModule],
   exports: [SidenavComponent]
 })
 export class SidenavModule {}
