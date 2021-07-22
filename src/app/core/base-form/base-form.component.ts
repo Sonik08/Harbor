471066311823bd0fa77c353cbf1b5e057b25d45e
Sonik08 <stanislavnovikov1@gmail.com>
2021-07-22 15:03:18 +0300

    CORE Adding base form component

--- src/app/core/base-form/base-form.component.ts
+++ src/app/core/base-form/base-form.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Model } from '../models/model';
+import { BaseFormVM } from '../vm/form.vm';
 
 @Component({
   selector: 'base-form',
   templateUrl: './base-form.component.html',
   styleUrls: ['./base-form.component.scss']
 })
-export class BaseFormComponent implements OnInit {
+export class BaseFormComponent<TModel extends Model> {
 
-  constructor() { }
+  @Input()saveable: boolean;
+  @Input()model: TModel;
+  @Input()form: FormGroup;
+  @Input()vm: BaseFormVM<TModel>
 
-  ngOnInit(): void {
-  }
-
+  onSubmit(){}
 }
--- src/app/core/base-form/base-form.module.ts
+++ src/app/core/base-form/base-form.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BaseFormComponent } from './base-form.component';
+import { MaterialModule } from 'src/app/pages/material.module';
 
 
 
 @NgModule({
   declarations: [BaseFormComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MaterialModule
+  ],
+  exports: [BaseFormComponent]
 })
 export class BaseFormModule { }
